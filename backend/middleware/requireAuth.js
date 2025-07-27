6fbeb959ea202f88a00efbdb3cbdeef2fd0854e8
femmytedrey <femmytedrey@gmail.com>
2025-07-27 18:11:09 +0100

    get token from headers as alternate to from cookies

--- backend/middleware/requireAuth.js
+++ backend/middleware/requireAuth.js
@@ -2,7 +2,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userSchema");
 
 const requireAuth = async (req, res, next) => {
-  const token = req.cookies.token;
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
 
   if (!token) {
     return res.status(401).json({ error: "Authorization is required" });
